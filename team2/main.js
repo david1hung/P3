2b2867bb6207eedff0d602388c7243602bfdc367
Tremaine Eto <tremaineeto@ucla.edu>
2016-05-04 00:34:54 -0700

    Added Profile Page CSS, JS, HTML as well as routing in main.js

--- team2/main.js
+++ team2/main.js
@@ -21,6 +21,10 @@ app.get('/worldOfWork', function(req, res) {
     res.render('worldOfWork.html', {});
 });
 
+app.get('/profile', function(req, res) {
+    res.render('profile.html', {});
+});
+
 app.post('/signup', function(req, res) {
     res.writeHead(501);
     res.end('501 - Not implemented');
@@ -64,10 +68,10 @@ app.get('/career/:occupation/world-of-work', function(req, res) {
     res.end('501 - Not implemented');
 });
 
-app.get('/profile', function(req, res) {
-    res.writeHead(501);
-    res.end('501 - Not implemented');
-});
+// app.get('/profile', function(req, res) {
+//     res.writeHead(501);
+//     res.end('501 - Not implemented');
+// });
 
 app.get('/browse', function(req, res) {
     res.writeHead(501);
