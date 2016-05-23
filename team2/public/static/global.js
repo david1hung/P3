d15e3107369dcefc4a1de8999fcc3a109e207f32
Roy Xia <royxia062@gmail.com>
2016-05-23 15:34:31 -0700

    Refined login UI

--- team2/public/static/global.js
+++ team2/public/static/global.js
@@ -1,21 +1,4 @@
-var initiallyHidden = [
-	'#fader',
-	'#signUpBox',
-	'#loginBox',
-	'#salaryDialog',
-	'#educationDialog',
-	'#skillsDialog',
-	'#careerOutlookDialog',
-	'#worldOfWorkDialog'
-];
-
 $(document).ready(function(){
-
-	var i;
-	for (i = 0; i < initiallyHidden.length; i++) {
-		$(initiallyHidden[i]).hide();
-	}
-
 	$('#signUpLogin').click(function(){
 
 		$('#fader').fadeIn();
@@ -67,52 +50,4 @@ $(document).ready(function(){
   			$(whichDialog).hide();
 		};  
 	})
-
-	// var chart = new CanvasJS.Chart("chartContainer", {
-	// 	title:{
-	// 		text: "My First Chart in CanvasJS"              
-	// 	},
-	// 	data: [              
-	// 	{
-	// 		// Change type to "doughnut", "line", "splineArea", etc.
-	// 		type: "column",
-	// 		dataPoints: [
-	// 			{ label: "apple",  y: 10  },
-	// 			{ label: "orange", y: 15  },
-	// 			{ label: "banana", y: 25  },
-	// 			{ label: "mango",  y: 30  },
-	// 			{ label: "grape",  y: 28  }
-	// 		]
-	// 	}
-	// 	]
-	// });
-	// chart.render();
-
-	$(function () {
-	    $('#container').highcharts({
-	        data: {
-	            table: 'salaryTable'
-	        },
-	        chart: {
-	            type: 'column'
-	        },
-	        title: {
-	            text: 'Occupational Salary Range'
-	        },
-	        yAxis: {
-	            allowDecimals: false,
-	            title: {
-	                text: 'Yearly salary'
-	            }
-	        },
-	        tooltip: {
-	            formatter: function () {
-	                return '<b>' + this.series.name + '</b><br/>$' +
-	                    this.point.y + ' ' + this.point.name.toLowerCase();
-	            }
-	        }
-	    });
-	});
-
-
 });
