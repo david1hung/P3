a4a40cc3d531434f90285608501205081e7eccf3
Roy Xia <royxia062@gmail.com>
2016-05-29 18:34:36 -0700

    Fixed random career not working. Cleaned up a few potential SQL injection vulnerabilities.

--- team2/models/occupation.js
+++ team2/models/occupation.js
@@ -23,9 +23,9 @@ module.exports.find = function(soc, successNext, errNext) {
         }
     }
 
-    queryString += "FROM Skills, Occupation WHERE Occupation.soc = '" + soc + "' && Skills.soc = '" + soc + "';";
+    queryString += "FROM Skills, Occupation WHERE Occupation.soc = ? && Skills.soc = ?;";
 
-    connection.query(queryString, function(err, rows, fields) {
+    connection.query(queryString, [soc, soc], function(err, rows, fields) {
         if (err === null && rows.length == 1) {
             successNext(rows[0]);
             connection.end();
@@ -76,7 +76,7 @@ module.exports.getSkills = function(soc, successNext, errNext) {
     var connection = mysql.createConnection(config);
     connection.connect();
 
-    connection.query("SELECT * FROM Skills WHERE soc = '" + soc + "'", function(err, rows, fields) {
+    connection.query("SELECT * FROM Skills WHERE soc = ?", [soc], function(err, rows, fields) {
         if (err === null && rows.length == 1) {
             successNext(rows[0]);
         } else {
@@ -97,7 +97,7 @@ module.exports.getRandomSOC = function(successNext, errNext) {
     var connection = mysql.createConnection(config);
     connection.connect();
 
-    connection.query('SELECT soc FROM occupation ORDER BY RAND() LIMIT 1;', function(err, rows, fields) {
+    connection.query('SELECT soc FROM Occupation ORDER BY RAND() LIMIT 1;', function(err, rows, fields) {
         if (err === null && rows.length == 1) {
             successNext(rows[0].soc);
         }
