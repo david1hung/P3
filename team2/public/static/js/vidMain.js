5e97e15b99a8629f9203243ae62bcb77fc7dd010
CalebGM <calebmayerski@cox.net>
2016-06-03 15:41:19 -0700

    like buttons

--- team2/public/static/js/vidMain.js
+++ team2/public/static/js/vidMain.js
@@ -49,10 +49,19 @@ function updateRank(choice) {
 		$.get('/career/' + soc + '/viddown');
 	}
 }
-function showNextCareerButton() {
-    $('.upthumb').addClass("upthumb-selected");
-  $('.upthumb').removeClass("upthumb");
-  $('#next-career').show("fast");
+function showNextCareerButton(thumb) {
+	if (thumb == "up") {
+		$('.upthumb').addClass("upthumb-selected");
+		$('.upthumb').removeClass("upthumb");
+	} else if (thumb == "mid") {
+		$('.midthumb').addClass("midthumb-selected");
+		$('.midthumb').removeClass("midthumb");
+	} else if (thumb == "down") {
+		$('.downthumb').addClass("downthumb-selected");
+		$('.downthumb').removeClass("downthumb");
+	}
+  
+	$('#next-career').show("fast");
   
 }
 
